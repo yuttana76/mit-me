d123c4127908686c4fcd53a7fcbe30d3423b6194
Yuttana <yuttana76>
2018-11-04 22:58:02 +0700

    Add user list , detail and submit button by <a>

--- src/app/views/trade/user-detail/user-detail.component.ts
+++ src/app/views/trade/user-detail/user-detail.component.ts
@@ -1,15 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { User } from '../model/user.model';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { UserFormService } from './userForm.service';
+import { DATE_REGEX, TIME_REGEX, stringsToDate } from '../trade.factory';
 
 @Component({
   selector: 'app-user-detail',
   templateUrl: './user-detail.component.html',
   styleUrls: ['./user-detail.component.scss']
 })
-export class UserDetailComponent implements OnInit {
+export class UserDetailComponent implements OnInit, OnDestroy {
+  validationMessages: any;
+  user: User = new User();
+  form: FormGroup;
+  formChangeSub: Subscription;
 
-  constructor() { }
+  constructor(
+    public userFormService: UserFormService
+  ) { }
 
   ngOnInit() {
+    this._buildForm();
+  }
+
+
+  private _buildForm() {
+   // Initial Form fields
+   this.form = new FormGroup({
+    loginName: new FormControl(null, {
+      validators: [Validators.required]
+    }),
+    firstName: new FormControl(null, {
+      // validators: [Validators.required]
+    }),
+    lastName: new FormControl(null, {
+      // validators: [Validators.required]
+    }),
+    dobDate: new FormControl(null, {
+      validators: [Validators.required
+         , Validators.pattern(DATE_REGEX)
+      ]
+    }),
+   });
+
+    // Subscribe to form value changes
+    this.formChangeSub = this.form
+    .valueChanges
+    .subscribe(data => this._onValueChanged());
+
+    // this._onValueChanged();
+  }
+
+  private _onValueChanged() {
+    if (!this.form) { return; }
+  }
+
+  ngOnDestroy() {
+    this.formChangeSub.unsubscribe();
+  }
+
+  onSubmit() {
+    console.log('SUBMITED ! ');
+  }
+
+  onSubmit2() {
+    console.log('SUBMITED 2! ');
+  }
+
+  onSubmit3() {
+    console.log('SUBMITED 3! ');
   }
 
 }
---
+++ src/app/views/trade/user-detail/userForm.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserFormService {
+
+  validationMessages: any;
+
+  // Formats
+  dateFormat = 'dd/mm/yyyy';
+  timeFormat = 'h:mm AM/PM';
+
+  // Label
+  from_title = 'User Detail';
+  Birth_Day_label = 'Birth Day (' + this.dateFormat + ') ';
+
+  constructor() { }
+
+}

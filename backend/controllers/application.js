9cb3c7f97ccbec59d56babe70713f4987a9d2767
Yuttana Khumnual <yuttana@mpam.local>
2018-11-09 11:53:56 +0700

    Add Application API  & Application service

---
+++ backend/controllers/application.js
@@ -0,0 +1,34 @@
+const dbConfig = require('./config');
+
+var config = dbConfig.dbParameters;
+
+
+exports.getApplication = (req, res, next) => {
+
+  var fncName = 'getAMC()';
+  var queryStr = `select * FROM MIT_ApplicationInfo`;
+
+  const sql = require('mssql')
+  const pool1 = new sql.ConnectionPool(config, err => {
+    pool1.request() // or: new sql.Request(pool1)
+    .query(queryStr, (err, result) => {
+        // ... error checks
+        if(err){
+          console.log( fncName +' Quey db. Was err !!!' + err);
+          res.status(201).json({
+            message: err,
+          });
+        }else {
+          res.status(200).json({
+            message: fncName + "Quey db. successfully!",
+            result: result.recordset
+          });
+        }
+    })
+  })
+
+  pool1.on('error', err => {
+    // ... error handler
+    console.log("EROR>>"+err);
+  })
+}

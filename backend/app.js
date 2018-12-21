6e53a91739cab6e1789c116710c13af77e5f598e
Yuttana Khumnual <yuttana@mpam.local>
2018-12-21 16:50:32 +0700

    Update server env dev & prod

--- backend/app.js
+++ backend/app.js
@@ -47,19 +47,19 @@ app.use("/", express.static(path.join(__dirname, "angular")));
 /*
 Config for separate Banckend and Frontend servers
 */
-// app.use((req, res, next) => {
-//   res.setHeader(
-//       "Access-Control-Allow-Origin",
-//       "*");
-//   res.setHeader(
-//       "Access-Control-Allow-Headers",
-//       "Origin, X-Requested-Width, Content-Type, Accept, Authorization");
-//   res.setHeader(
-//       "Access-Control-Allow-Methods",
-//       "GET, POST, PATCH, PUT,  DELETE, OPTIONS"
-//   );
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader(
+      "Access-Control-Allow-Origin",
+      "*");
+  res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-Width, Content-Type, Accept, Authorization");
+  res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, PUT,  DELETE, OPTIONS"
+  );
+  next();
+});
 
 app.use("/api/connex",connexRoutes);
 app.use("/api/fund",fundRoutes);

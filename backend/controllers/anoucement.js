a37d5507f527664fa8b3c2b827348b9b9857b4ab
Yuttana Khumnual <yuttana@mpam.local>
2019-01-07 15:33:16 +0700

    Finish on create anoucement service

--- backend/controllers/anoucement.js
+++ backend/controllers/anoucement.js
@@ -2,7 +2,6 @@ const dbConfig = require('../config/db-config');
 var config = dbConfig.dbParameters;
 
 exports.getAnoucement = (req, res, next) => {
-
   var fncName = 'getAnoucement()';
   var queryStr = `SELECT * FROM MIT_ANOUCEMENT  order by AnouceDate `;
 
@@ -24,7 +23,38 @@ exports.getAnoucement = (req, res, next) => {
         }
     })
   })
+  pool1.on('error', err => {
+    // ... error handler
+    console.log("EROR>>"+err);
+  })
+}
 
+
+exports.getActiveAnoucement = (req, res, next) => {
+  var fncName = 'getAnoucement()';
+  var queryStr = `SELECT * FROM MIT_ANOUCEMENT
+                  WHERE status='A'
+                  and AnouceDate <= GETDATE()
+                  order by AnouceDate `;
+
+  const sql = require('mssql')
+  const pool1 = new sql.ConnectionPool(config, err => {
+    pool1.request() // or: new sql.Request(pool1)
+    .query(queryStr, (err, result) => {
+        // ... error checks
+        if(err){
+          console.log( fncName +' Quey db. Was err !!!' + err);
+          res.status(201).json({
+            message: err,
+          });
+        }else {
+          res.status(200).json({
+            message: fncName + "Quey db. successfully!",
+            result: result.recordset
+          });
+        }
+    })
+  })
   pool1.on('error', err => {
     // ... error handler
     console.log("EROR>>"+err);
--- backend/routes/anoucement.js
+++ backend/routes/anoucement.js
@@ -6,10 +6,10 @@ const anoucementController = require('../controllers/anoucement')
 
 // router.get("", checkAuth, fundController.getFunds);
 router.get("", anoucementController.getAnoucement);
+router.get("/active", anoucementController.getActiveAnoucement);
 router.post("", anoucementController.addAnoucement);
 router.put("", anoucementController.updateAnoucement);
 router.delete("/:id", anoucementController.delAnoucement);
-
 
 
 module.exports = router;

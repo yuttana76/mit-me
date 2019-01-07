298d99eee0899705a6e7b941bf17bbb17146db0b
Yuttana Khumnual <yuttana@mpam.local>
2019-01-07 14:43:18 +0700

    Finish on create Anoucement API

---
+++ backend/routes/anoucement.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const router = express.Router();
+const checkAuth = require('../middleware/check-auth');
+const anoucementController = require('../controllers/anoucement')
+
+// router.get("", checkAuth, fundController.getFunds);
+router.get("", anoucementController.getAnoucement);
+router.post("", anoucementController.addAnoucement);
+router.put("", anoucementController.updateAnoucement);
+router.delete("/:id", anoucementController.delAnoucement);
+
+
+
+module.exports = router;

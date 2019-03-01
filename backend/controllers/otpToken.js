2300616545edf9c792733b445ad1eb70df65db92
Yuttana Khumnual <yuttana@mpam.local>
2019-03-01 18:27:58 +0700

    Add API getOTPtokenToSMS() not finish yet

--- backend/controllers/otpToken.js
+++ backend/controllers/otpToken.js
@@ -28,6 +28,28 @@ let totp = new OTPAuth.TOTP({
 /*
 Send mail  by OTP
 */
+
+exports.getOTPtokenToSMS = (req, res, next) => {
+var options = {
+  url: 'https://api.github.com/users/narenaryan',
+  headers: {
+      'User-Agent': 'request'
+  }
+};
+// Return new promise
+return new Promise(function(resolve, reject) {
+// Do async job
+  request.get(options, function(err, resp, body) {
+      if (err) {
+          reject(err);
+      } else {
+          resolve(JSON.parse(body));
+      }
+  })
+})
+}
+
+
 exports.getOTPtokenToMail = (req, res, next) => {
 
     const _pid = req.body.pid

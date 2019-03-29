ccaac66e95fab383b3b7667c4fc5659b26ef4cac
Yuttana Khumnual <yuttana@mpam.local>
2019-03-29 17:53:05 +0700

    Set backend server is https

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const app = require("./app");
 const debug = require("debug")("node-angular");
 const http = require("http");
-
+const https = require('https');
+const fs = require('fs');
 
 const normalizePort = val => {
   var port = parseInt(val, 10);
@@ -49,11 +50,26 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const port = normalizePort(process.env.PORT || "3000");
-console.log('PORT=' +process.env.PORT +';->'+port);
+console.log('NODE_ENV=' +process.env.NODE_ENV );
+console.log('PORT='+port);
 
 app.set("port", port);
 
-const server = http.createServer(app);
-server.on("error", onError);
-server.on("listening", onListening);
-server.listen(port);
+//HTTP
+// const server = http.createServer(app);
+// server.on("error", onError);
+// server.on("listening", onListening);
+// server.listen(port);
+
+console.log('DIR>' + __dirname);
+
+// //HTTPS
+const option = {
+  key: fs.readFileSync(__dirname+'/merchantasset_CA/key.pem'),
+  cert: fs.readFileSync(__dirname+'/merchantasset_CA/cert.pem'),
+    passphrase: 'mpam@2019'
+};
+var server = https.createServer(option, app)
+.listen(port,function () {
+  console.log("Listening on port https://localhost:%s", server.address().port);
+})

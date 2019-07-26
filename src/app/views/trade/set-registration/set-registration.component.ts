d5834131d9656a239aaf0b10c52396227735e248
Yuttana Khumnual <yuttana@mpam.local>
2019-07-26 10:33:24 +0700

    Finish on set-registration comp

--- src/app/views/trade/set-registration/set-registration.component.ts
+++ src/app/views/trade/set-registration/set-registration.component.ts
@@ -19,6 +19,24 @@ export class SetRegistrationComponent implements OnInit {
   fillFormGroup: FormGroup;
   register = new RegisterModel();
 
+  dismissible = true;
+  defaultAlerts: any[] = [
+    // {
+    //   type: 'success',
+    //   msg: `You successfully read this important alert message.`
+    // },
+    // {
+    //   type: 'info',
+    //   msg: `This alert needs your attention, but it's not super important.`
+    // },
+    // {
+    //   type: 'danger',
+    //   msg: `Better check yourself, you're not looking too good.`
+    // }
+  ];
+  alerts = this.defaultAlerts;
+
+
   constructor(private _formBuilder: FormBuilder,
     private toastr: ToastrService,
     private confirmationDialogService: ConfirmationDialogService,
@@ -36,34 +54,40 @@ export class SetRegistrationComponent implements OnInit {
 
   }
 
+  onClosedAlert(dismissedAlert: any): void {
+    this.alerts = this.alerts.filter(alert => alert !== dismissedAlert);
+  }
+
   onSubmit(){
 
+    console.log("FORM VALID >>" + this.fillFormGroup.valid);
 
-    this.utilService.regisToMailService(this.register.firstName,this.register.lastName,this.register.mobile,this.register.email) .subscribe(data =>{
-      this.toastr.success("Register successful." , "Successful", {
-        timeOut: 3000,
-        closeButton: true,
-        positionClass: "toast-top-center"
-      });
+    if(this.fillFormGroup.valid){
 
-      this.spinnerLoading = false;
-    }
-    , error => {
-
-        this.toastr.error("Register incomplete  message." + JSON.stringify(error) , "Error", {
-          timeOut: 6000,
+      this.utilService.regisToMailService(this.register.firstName,this.register.lastName,this.register.mobile,this.register.email) .subscribe(data =>{
+        this.toastr.success("Register successful." , "Successful", {
+          timeOut: 3000,
           closeButton: true,
           positionClass: "toast-top-center"
         });
 
-        console.log("WAS ERR>>" + JSON.stringify(error) );
         this.spinnerLoading = false;
       }
-    );
+      , error => {
 
+          this.toastr.error("Register incomplete  message." + JSON.stringify(error) , "Error", {
+            timeOut: 6000,
+            closeButton: true,
+            positionClass: "toast-top-center"
+          });
 
+          console.log("WAS ERR>>" + JSON.stringify(error) );
+          this.spinnerLoading = false;
+        }
+      );
+    }else{
 
-
+    }
   }
 
 }

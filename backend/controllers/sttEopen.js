572f254981a08bf7dd2310bddf1542f084066de4
yuttana76 <yuttana76@gmail.com>
2020-11-24 17:48:53 +0700

    Create brokerLogin() Function

--- backend/controllers/sttEopen.js
+++ backend/controllers/sttEopen.js
@@ -1,43 +1,57 @@
 /*
 https://nodejs.org/api/crypto.html#crypto_sign_sign_privatekey_outputencoding
-
 */
 
 const mpamConfig = require('../config/mpam-config');
 var logger = require('../config/winston');
 var prop = require("../config/backend-property");
+
 const crypto = require('crypto');
+const fs = require('fs');
 
 const { validationResult } = require('express-validator');
+
+
+const { generateKeyPairSync } = require('crypto');
+
+
+const { privateKey, publicKey } = crypto.generateKeyPairSync('ec', {
+  namedCurve: 'sect239k1',
+  publicKeyEncoding:  { type: 'spki', format: 'pem' },
+  privateKeyEncoding: { type: 'pkcs8', format: 'pem' }
+});
+
+
+const option = {
+  publicKey: fs.readFileSync(process.env.EOPEN_PUBLIC_KEY),
+  privateKey: fs.readFileSync(process.env.EOPEN_PRIVATE_KEY),
+};
+
 
 exports.brokerLogin = (req,res,next)=>{
 
   logger.info("Welcome API brokerLogin/");
 
-  var moment = require('moment')
-
-
 
   // "requestTime":"yyyyMMddHHmmss",
+  var moment = require('moment')
   var requestTime = moment().format('YYYYMMDDHHmmss')
   console.log(requestTime);
 
-  // "signature":"RSA_SHA256(privateKey, yyyyMMddHHmmss)"
-  const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
-    modulusLength: 2048,
-  });
+  // signature
+  const sign = crypto.createSign('SHA256')
+  sign.write(requestTime)
+  sign.end()
+  const signature = sign.sign(option.privateKey, 'hex')
 
-  const sign = crypto.createSign('SHA256');
-  sign.update(requestTime);
-  sign.end();
-  const signature = sign.sign(privateKey);
 
   const verify = crypto.createVerify('SHA256');
-  verify.update(requestTime);
+  verify.write(requestTime);
   verify.end();
+  verifyResult =  verify.verify(option.publicKey, signature,'hex')
 
-  verifyResult = verify.verify(publicKey, signature)
-  // Prints: true
+
+  console.log(verifyResult)
 
   res.status(200).json({
     code: '000',

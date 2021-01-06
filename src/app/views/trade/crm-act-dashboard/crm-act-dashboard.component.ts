0677c3917a633da4664f053ff2a94ad9013d51e8
yuttana76@gmail.com <yuttana76@gmail.com>
2021-01-06 23:22:07 +0700

    Create CRM screens

--- src/app/views/trade/crm-act-dashboard/crm-act-dashboard.component.ts
+++ src/app/views/trade/crm-act-dashboard/crm-act-dashboard.component.ts
@@ -45,17 +45,66 @@ const colors: any = {
   styleUrls: ['./crm-act-dashboard.component.scss']
 })
 export class CrmActDashboardComponent implements OnInit {
-
-
   @ViewChild('modalContent', {static: false})
+// lineChart
+public lineChartData: Array<any> = [
+  {data: [65, 59, 80, 81, 56, , ], label: 'Series A'},
+  {data: [28, 48, 40, 19, 86, , ], label: 'Series B'},
+  {data: [18, 48, 77, 9, 100, , ], label: 'Series C'}
+];
+public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+public lineChartOptions: any = {
+  animation: false,
+  responsive: true
+};
+public lineChartColours: Array<any> = [
+  { // grey
+    backgroundColor: 'rgba(148,159,177,0.2)',
+    borderColor: 'rgba(148,159,177,1)',
+    pointBackgroundColor: 'rgba(148,159,177,1)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+  },
+  { // dark grey
+    backgroundColor: 'rgba(77,83,96,0.2)',
+    borderColor: 'rgba(77,83,96,1)',
+    pointBackgroundColor: 'rgba(77,83,96,1)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgba(77,83,96,1)'
+  },
+  { // grey
+    backgroundColor: 'rgba(148,159,177,0.2)',
+    borderColor: 'rgba(148,159,177,1)',
+    pointBackgroundColor: 'rgba(148,159,177,1)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+  }
+];
+public lineChartLegend = true;
+public lineChartType = 'line';
+
+// barChart
+public barChartOptions: any = {
+  scaleShowVerticalLines: false,
+  responsive: true
+};
+public barChartLabels: string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+public barChartType = 'bar';
+public barChartLegend = true;
+
+public barChartData: any[] = [
+  {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+  {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+];
+
+
   modalContent: TemplateRef<any>;
-
   view: CalendarView = CalendarView.Month;
-
   CalendarView = CalendarView;
-
   viewDate: Date = new Date();
-
   modalData: {
     action: string;
     event: CalendarEvent;
--- src/app/views/trade/crm-personal-search/crm-personal-search.component.ts
+++ src/app/views/trade/crm-personal-search/crm-personal-search.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { PageEvent } from '@angular/material';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-crm-personal-search',
@@ -11,12 +13,28 @@ export class CrmPersonalSearchComponent implements OnInit {
   spinnerLoading = false;
   searchForm: FormGroup;
 
+  currentPage = 1;
+  rowsPerPage = 20;
+  totalRecords = 10;
+  pageSizeOptions = [10, 20, 50, 100];
+
+  cust_list=[{'Cust_Code':'123', 'FullName':'Mr. xxxx','Aliast':'นาย-A','RM':'HASUN','LastAct':'Open acc.','Action':''}
+   ,{'Cust_Code':'456', 'FullName':'Mr. yyyy','Aliast':'นาย ข','RM':'Tananya','LastAct':'Order .','Action':''}
+   ,{'Cust_Code':'789', 'FullName':'Mr. zzzz','Aliast':'นาย ง','RM':'Rosakorn','LastAct':'Offer bond','Action':''}
+   ,{'Cust_Code':'000', 'FullName':'Mr. None','Aliast':'นาย จ','RM':'Supasiri','LastAct':'Call report','Action':''}
+  
+  ];
+
+  cust_displayedColumns: string[] = ['Cust_Code', 'FullName','Aliast','RM','LastAct','Action'];
+  cust_dataSource = new BehaviorSubject(this.cust_list);
+
+
   constructor() { }
 
   ngOnInit() {
     this.searchForm = new FormGroup({
       custId: new FormControl(null, {
-        validators: [Validators.required]
+        // validators: [Validators.required]
       }),
       // custType: new FormControl(null, {
       //   validators: [Validators.required]
@@ -41,4 +59,21 @@ export class CrmPersonalSearchComponent implements OnInit {
     // subject.onNext([]);
     // this.dataSource.next([]);
   }
+
+
+  onChangedPage(pageData: PageEvent) {
+    // console.log(pageData);
+    // this.spinnerLoading = true;
+    this.currentPage =  pageData.pageIndex + 1;
+    this.rowsPerPage =  pageData.pageSize;
+
+    // console.log('Condition>>', this.conditions);
+    // this.customerService.getCustomers(this.rowsPerPage, this.currentPage, this.conditions);
+    // this.postsSub = this.customerService.getCustomerUpdateListener().subscribe((customers: Customer[]) => {
+    //       this.spinnerLoading = false;
+    //       this.customers = customers;
+    //       this.dataSource.next(this.customers);
+    //   });
+  }
+
 }

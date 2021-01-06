bc4cee11d92a17dffa18ab8ad5bf4b124cab35a8
yuttana76@gmail.com <yuttana76@gmail.com>
2021-01-06 13:09:10 +0700

    Create crm customer search screen

--- src/app/views/trade/crm-personal-search/crm-personal-search.component.ts
+++ src/app/views/trade/crm-personal-search/crm-personal-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-crm-personal-search',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrmPersonalSearchComponent implements OnInit {
 
+  spinnerLoading = false;
+  searchForm: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    this.searchForm = new FormGroup({
+      custId: new FormControl(null, {
+        validators: [Validators.required]
+      }),
+      // custType: new FormControl(null, {
+      //   validators: [Validators.required]
+      // }),
+    });
   }
 
+
+  onSerachCust() {
+
+    // console.log('onSerachCust ! ');
+    if (this.searchForm.invalid) {
+      // console.log('form.invalid() ' + this.form.invalid);
+      return true;
+    }
+    this.spinnerLoading = true;
+  }
+
+  onReset(){
+    // this.customers = [];
+
+    // subject.onNext([]);
+    // this.dataSource.next([]);
+  }
 }

8403bb245a1f31c0f37974759ba884949b1f5174
yuttana76@gmail.com <yuttana76@gmail.com>
2021-01-24 13:49:26 +0700

    Create on task screen

--- src/app/views/trade/crm-task/crm-task.component.ts
+++ src/app/views/trade/crm-task/crm-task.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { DatePipe, Location } from '@angular/common';
 import { MatSelectChange } from '@angular/material';
+import { CrmTask } from '../model/crmTask.model';
 
 @Component({
   selector: 'app-crm-task',
@@ -10,13 +11,14 @@ import { MatSelectChange } from '@angular/material';
 })
 export class CrmTaskComponent implements OnInit {
 
-  firstFormGroup: FormGroup;
+  schFormGroup: FormGroup;
+  crmTaskObj: CrmTask = new CrmTask();
 
   actTypeList =[
     {
-    code:'callReport',
-    desc:'Call report',
-  },
+      code:'0',
+      desc:'Please choose',
+    },
   {
     code:'contactCustAct',
     desc:'ติดต่อลูกค้า	Contact ',
@@ -47,6 +49,105 @@ export class CrmTaskComponent implements OnInit {
   // },
 ];
 
+channelList =[ {
+  code:'tel',
+  desc:'โทรศัพท์',
+},
+{
+  code:'email',
+  desc:'E-mail',
+},
+{
+  code:'Social',
+  desc:'Social',
+},
+{
+  code:'mail',
+  desc:'จดหมาย',
+},
+];
+
+prodGroupList=[
+  {
+    code:'000',
+    desc:'ไม่ระบุ',
+  },
+  {
+    code:'bond',
+    desc:'Bond',
+  },
+  {
+    code:'mf',
+    desc:'MF',
+  },
+  {
+    code:'pf',
+    desc:'PF',
+  },
+];
+
+
+productList=[
+  {
+    code:'000',
+    desc:'ไม่ระบุ',
+  },
+  {
+    code:'1',
+    desc:'Chaiyo',
+  },
+  {
+    code:'2',
+    desc:'JMT',
+  },
+  {
+    code:'3',
+    desc:'PTT-w3',
+  },
+  {
+    code:'4',
+    desc:'THAI',
+  },
+  {
+    code:'5',
+    desc:'PTTGC',
+  },
+]
+
+feedbackResultList=[
+  {
+    code:'1',
+    desc:'ลงทุน',
+  },
+  {
+    code:'2',
+    desc:'ไม่ลงทุน',
+  },
+  {
+    code:'3',
+    desc:'ติดตามต่อ',
+  },  
+]
+
+feedbackNotList=[
+  {
+    code:'1',
+    desc:'ไม่น่าสนใจ',
+  },
+  {
+    code:'2',
+    desc:'มีความเสี่ยงสูง',
+  },
+  {
+    code:'3',
+    desc:'ลงทุนนานเงินไป',
+  },  
+  {
+    code:'4',
+    desc:'ไม่ชอบลงทุนต่างประเทศ',
+  },  
+]
+
   constructor(
     private _formBuilder: FormBuilder,
     private location: Location,
@@ -54,27 +155,27 @@ export class CrmTaskComponent implements OnInit {
 
   ngOnInit() {
 
-    this.firstFormGroup = this._formBuilder.group({
+    this.schFormGroup = this._formBuilder.group({
       // firstCtrl: ['', Validators.required]
 
-      actType: new FormControl(null, {
-        validators: [Validators.required]
-      }),
+      schType: new FormControl(null, {validators: [Validators.required]}),
+      // schStatus: new FormControl(null, {validators: [Validators.required]}),
+      schStartDate: new FormControl(null, {validators: [Validators.required]}),
+      
+      note: new FormControl(null, {validators: [Validators.required]}),
+      
+      channel: new FormControl(null, { }),
+      prodCate: new FormControl(null, { }),
+      productItem: new FormControl(null, { }),
 
-      actStatus: new FormControl(null, {
-        validators: [Validators.required]
-      }),
-      remindDateTime: new FormControl(null, { }),
-      description: new FormControl(null, {
-        validators: [Validators.required
-        ]
-      }),
-      feedback: new FormControl(null, {
-        validators: [Validators.required]
-      }),
-      feedbackOth: new FormControl(null, { }),
-      attachFiles: new FormControl(null, {  }),
-      assignTo: new FormControl(null, {  }),
+      schCloseDate: new FormControl(null, { }),
+      feedbackRS: new FormControl(null, { }),
+      feedbackReason: new FormControl(null, { }),
+      feedbackNote: new FormControl(null, { }),
+      investType: new FormControl(null, { }),
+      investValue: new FormControl(null, { }),
+      investDate: new FormControl(null, { }),
+      
 
 
     });
---
+++ src/app/views/trade/model/crmTask.model.ts
@@ -0,0 +1,27 @@
+export class CrmTask {
+    public CompCode: string;
+    public CustCode: string;
+  
+    public schType: string;
+    public schStartDate: string;
+    public note: string;
+    public channel: string;
+    public prodCate: string;
+    public productItem: string;
+    public schCloseDate: string;
+    public feedbackRS: string;
+    public feedbackReason: string;
+    public feedbackNote: string;
+    public investType: string;
+    public investValue: string;
+    public investDate: string;
+  
+    public CreateBy: string;
+    public CreateDate: string;
+    public UpdateBy: string;
+    public UpdateDate: string;
+
+    constructor(){}
+  
+  }
+  

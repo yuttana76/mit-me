110c60741b5231852b76d4b70725123da9f032f1
yuttana76@gmail.com <yuttana76@gmail.com>
2021-01-24 23:40:42 +0700

    Update CRM screens

--- src/app/views/trade/crm-personal-data/crm-personal-data.component.ts
+++ src/app/views/trade/crm-personal-data/crm-personal-data.component.ts
@@ -13,64 +13,35 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class CrmPersonalDataComponent implements OnInit, OnDestroy {
 
-  // calendarOptions: CalendarOptions = {
-  //   initialView: 'dayGridMonth'
-  // };
-
   form: FormGroup;
   paramId: String = '';
   customer: CrmPersonModel = new CrmPersonModel();
 
-
-
-  // All Doughnut
-  public alldoughnutChartLabels: string[] = ['Private Fund', 'Bond', 'LBDU'];
-  public alldoughnutChartData: number[] = [350, 450, 100];
-  public alldoughnutChartType = 'doughnut';
-
-  // LBDU Doughnut
-  public lbdu_DoughnutChartLabels: string[] = ['KF-RMF', 'KFGTECHRMF', 'KF-SSF','TISCO-SSF','KBANK-SSF'];
-  public lbdu_DoughnutChartData: number[] = [100000.00, 144059.82, 20569.98,50123.98,5123.98];
-  public lbdu_DoughnutChartType = 'doughnut';
-
-
-   lbdu_list=[{'Fund_Code':'KF-RMF', 'NAV':'10.0000','MKT_Value':'100,000.00'}
-   ,{'Fund_Code':'KFGTECHRMF', 'NAV':'15.123','MKT_Value':'144,059.82'}
-   ,{'Fund_Code':'KF-SSF', 'NAV':'11.6336','MKT_Value':'20,569.98'}
-   ,{'Fund_Code':'TISCO-SSF', 'NAV':'15.6336','MKT_Value':'50,123.98'}
-   ,{'Fund_Code':'KBANK-SSF', 'NAV':'10.6336','MKT_Value':'5123.98'}
+   lbdu_list=[{'Code':'KF-RMF','Val':'100,000.00'}
+   ,{'Code':'KFGTECHRMF', 'Val':'144,059.82'}
+   ,{'Code':'KF-SSF', 'Val':'20,569.98'}
+   ,{'Code':'TISCO-SSF', 'Val':'50,123.98'}
+   ,{'Code':'KBANK-SSF', 'Val':'5123.98'}
   ];
 
-  lbdu_displayedColumns: string[] = ['Fund_Code', 'NAV','MKT_Value'];
+  lbdu_displayedColumns: string[] = ['Code', 'Val'];
   lbdu_dataSource = new BehaviorSubject(this.lbdu_list);
 
-  // Private Doughnut
-  public private_DoughnutChartLabels: string[] = ['PTT','KBANK'];
-  public private_DoughnutChartData: number[] = [4400, 10000];
-  public private_DoughnutChartType = 'doughnut';
-
-
-  private_list=[{'Symbol':'PTT', 'Val':'100','Avg':'40','Market':'44','Amount':'4,000','MarketVal':'4,400'}
-   ,{'Symbol':'KBANK', 'Val':'100','Avg':'90','Market':'100','Amount':'9,000','MarketVal':'10,000'}
+  private_list=[{'Code':'PF001', 'Val':'20,000,000'}
+   ,{'Code':'PF002', 'Val':'50,000,000'}
   
   ];
 
-  private_displayedColumns: string[] = ['Symbol', 'Val','Avg','Market','Amount','MarketVal'];
+  private_displayedColumns: string[] = ['Code', 'Val'];
   private_dataSource = new BehaviorSubject(this.private_list);
 
-  // Bond Doughnut
-  public bond_DoughnutChartLabels: string[] = ['CHAIYO','SANSIRI','MAGNOLIA'];
-  public bond_DoughnutChartData: number[] = [1000000, 500000,10000000];
-  public bond_DoughnutChartType = 'doughnut';
-
-
-  bond_list=[{'Symbol':'CHAIYO', 'Amount':'1,000,000'}
-   ,{'Symbol':'SANSIRI', 'Amount':'500,000'}
-   ,{'Symbol':'MAGNOLIA', 'Amount':'10,000,000'}
+  bond_list=[{'Code':'CHAIYO', 'Val':'1,000,000'}
+   ,{'Code':'SANSIRI', 'Val':'2,000,000'}
+   ,{'Code':'MAGNOLIA', 'Val':'10,000,000'}
   
   ];
 
-  bond_displayedColumns: string[] = ['Symbol', 'Amount'];
+  bond_displayedColumns: string[] = ['Code', 'Val'];
   bond_dataSource = new BehaviorSubject(this.bond_list);
 
 
